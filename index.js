025ba8bd358d243107ceffe0c56a5bc19a164db4
Megan Mai <59344390+megan-mai@users.noreply.github.com>
2023-07-28 11:28:44 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,146 @@
+var work = document.getElementById("work");
+var about = document.getElementById("about");
+var aboutText = document.getElementById("about-text");
+var workText = document.getElementById("work-text");
+var open = true;
+
+  work.addEventListener("click", function() {
+
+    if (workText.style.maxHeight) {
+        workText.style.maxHeight = null;
+        work.style.fontStyle = "normal";
+
+
+
+    } else {
+        workText.style.maxHeight = workText.scrollHeight + "px";
+        workText.style.transition = 'max-height .2s linear'
+        aboutText.style.maxHeight = null;
+        work.style.fontStyle = "italic";
+        about.style.fontStyle = "normal";
+
+    }
+  });
+
+
+  about.addEventListener("click", function() {
+    if (aboutText.style.maxHeight) {
+        aboutText.style.maxHeight = null;
+        about.style.fontStyle = "normal";
+
+
+
+    } else {
+        aboutText.style.maxHeight = workText.scrollHeight + "px";
+        workText.style.maxHeight = null;
+        about.style.fontStyle = "italic";
+        work.style.fontStyle = "normal";
+
+
+
+    }
+  });
+  $(document).click(function(e) {
+	if (!$(e.target).is('.panel-body')) {
+    	$('.collapse').collapse('hide');	    
+    }
+
+
+});
+
+
+// $('#backgroundCarousel').on('slid.bs.carousel', function () {
+//     var bio = document.getElementById("bio");
+//     var indicators = document.getElementsByClassName("carousel-indicators");
+//     var active = document.getElementsByClassName("carousel-item active");
+    
+//     var slideNumber = active[0].id;
+    
+//     if (slideNumber == 'first-slide'){
+//         bio.style.color = 'black';
+        
+
+    
+//     } else if (slideNumber == "second-slide"){
+//         bio.style.color = 'white';
+
+//         // indicators[0].innerHTML = "○";
+//         // indicators[1].innerHTML = "●";
+//         // indicators[2].innerHTML = "○";
+
+    
+//     }else if (slideNumber == "third-slide"){
+//         bio.style.color = 'white';
+//     }
+//     else{
+//         null;
+//     };  
+// })
+
+
+
+$('#backgroundCarousel').on('slid.bs.carousel', function () {
+    var bio = document.getElementById("bio");
+    var indicators = document.getElementsByClassName("carousel-indicators")[0].getElementsByTagName("li");
+    var slides = document.getElementsByClassName("carousel-item");
+    const colors = ["black","white","white"];
+            
+    for(let i=0; i<slides.length; i++){
+        if(slides[i].classList.contains('active')){
+           bio.style.color = colors[i];
+            for(let i =0; i < 3; i++){
+             indicators[i].style.backgroundColor = 'transparent';
+            }
+           indicators[i].style.backgroundColor = colors[i];;
+
+        }
+
+    }
+})
+
+
+
+
+$('#backgroundCarousel').on('slid.bs.carousel', function () {
+    var indicators = document.getElementsByClassName("ol.carousel-indicators");
+    const colors = ["black","white","white"];
+    
+
+    for(let i=0; i<indicators.length; i++){
+        if(indicators[i].classList.contains('active')){
+           indicators[i].style.backgroundColor = "black !important";
+        }
+    }
+})
+
+$('.carousel').carousel({
+    interval: 10000
+  })
+
+var $carousel = $('#backgroundCarousel');
+$carousel.carousel();
+var handled=false;
+//global variable
+
+$carousel.bind('slide.bs.carousel', function (e) {
+    var current=$(e.target).find('.carousel-item.active');
+    var indx=$(current).index();
+    if((indx+2)>$('.carousel-indicators li').length)
+        indx=-1
+     if(!handled)
+     {
+        $('.carousel-indicators li').removeClass('active')
+        $('.carousel-indicators li:nth-child('+(indx+2)+')').addClass('active');
+     }
+     else
+     {
+        handled=!handled;//if handled=true make it back to false to work normally.
+     }
+});
+
+$(".carousel-indicators li").on('click',function(){
+   //Click event for indicators
+   $(this).addClass('active').siblings().removeClass('active');
+   //remove siblings active class and add it to current clicked item
+   handled=true; //set global variable to true to identify whether indicator changing was handled or not.
+});
