2c4a7ad001b5bf3fb593c7e12d1697c5119638e5
Megan Mai <59344390+megan-mai@users.noreply.github.com>
2023-08-05 20:12:53 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -61,16 +61,18 @@ $('#backgroundCarousel').on('slid.bs.carousel', function () {
             
             //set all indicators transparent
             for(let j =0; j < 3; j++){
-                indicators[j].style.backgroundColor = 'transparent';
-                indicators[j].style.borderColor = colors[i];
+                // indicators[j].style.backgroundColor = 'transparent';
+                // indicators[j].style.borderColor = colors[i];
             }
 
             //set color of text based on background img
-            bio.style.color = colors[i];
+            // bio.style.color = colors[i];
             
 
             //set active indicator to solid
-            indicators[i].style.backgroundColor = colors[i] ;
+            // indicators[i].style.backgroundColor = colors[i] ;
+            // indicators[i].style.backgroundColor = 'white !important';
+
         }
     }
 })
